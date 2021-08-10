b2b9511ce982ec8503642e7bf5af800cfac0cb48
bhavesh-pepcoding <bhavesh.bansal@pepcoding.com>
2021-08-10 21:22:15 +0530

    fs

---
+++ fs/fs.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+// console.log(fs.readFileSync("a.txt","utf-8"));
+// console.log(fs.existsSync("a.txt"));
+// console.log(fs.existsSync("b.txt"));
+
+
+// fs.writeFileSync("a.txt", "how are you?");
+// fs.writeFileSync("b.txt","hello my name is bhavesh")
+
+// let file1name = process.argv[2];
+let flag = process.argv[3];
+if(flag == "-ar") {
+
+} else if(flag == "-aw") {
+
+}
+// let file2name = process.argv[4];
+
+// let file1data = fs.readFileSync(file1name,"utf-8");
+// let file2data = fs.readFileSync(file2name,"utf-8");
+// console.log(file1data + "\n" + file2data);
